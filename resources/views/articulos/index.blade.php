f854e544d8f40d91c444dc9e554f1bfdf6cab8fd
Ikeregibide <iker.gomez.rodriguez@ikasle.egibide.org>
2023-01-18 10:58:39 +0100

    cambio

--- resources/views/articulos/index.blade.php
+++ resources/views/articulos/index.blade.php
@@ -4,7 +4,7 @@
     <title>RevistApp</title>
 </head>
 <body>
-    <h1>asdf</h1>
+    <h1>Prueba</h1>
     <h2>Listado artículos:</h2>
     <a href="{{ route('articulos.create') }}">Crear nuevo</a>
     <table>
